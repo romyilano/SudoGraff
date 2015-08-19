c2de767f2d5cc3a789719f7a6b8be36c06c3d5bc
Romy Ilano <romy@snowyla.com>
2015-08-18 17:56:23 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# SudoGraff
+
+Hacking some drawing code from the awesome RayW Site. 
+Instead of just a drawing on a blank slate module,
+we are going to have people draw on live video and then take a snapshot
+
+This will be used for good natured mischeviousness (leaving virtual
+graffiti, doing live drawing videos, and even teaching and learning)
+to leave in various "locations"
+
+Would also like to use finger sticker drawing  / stickers for learning
+how to use technical equipment like the shop, or enabling people to leave instructions. Let's see how it goes.
+
+# Project
+
+part of SudoCare [https://sudoroom.org/wiki/SudoCare](https://sudoroom.org/wiki/SudoCare) 
---
+++ SudoGraff/SettingsViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+  @IBOutlet weak var sliderBrush: UISlider!
+  @IBOutlet weak var sliderOpacity: UISlider!
+
+  @IBOutlet weak var imageViewBrush: UIImageView!
+  @IBOutlet weak var imageViewOpacity: UIImageView!
+
+  @IBOutlet weak var labelBrush: UILabel!
+  @IBOutlet weak var labelOpacity: UILabel!
+
+  @IBOutlet weak var sliderRed: UISlider!
+  @IBOutlet weak var sliderGreen: UISlider!
+  @IBOutlet weak var sliderBlue: UISlider!
+
+  @IBOutlet weak var labelRed: UILabel!
+  @IBOutlet weak var labelGreen: UILabel!
+  @IBOutlet weak var labelBlue: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func close(sender: AnyObject) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+
+  @IBAction func colorChanged(sender: UISlider) {
+  }
+
+  @IBAction func sliderChanged(sender: UISlider) {
+  }
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+
+}
